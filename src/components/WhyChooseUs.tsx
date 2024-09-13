78837a990124b0a99b417ea55b9eb6c0af19691c
shailendrathakur300 <shailendrathakuracademic123@gmail.com>
2024-09-13 10:59:10 +0530

    done

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -9,16 +9,6 @@ const musicSchoolContent = [
       "Embark on a creative journey that’s uniquely yours. Our personalized design mentorship adapts to your individual style, setting the foundation for growth and innovation. At our design studio, your artistic aspirations align with our dedicated support, creating a seamless path to mastery.",
   },
   {
-    title: "Unleash Your Creative Vision: A Personal Journey in Design Mastery",
-    description:
-      "Embark on a creative journey that’s uniquely yours. Our personalized design mentorship adapts to your individual style, setting the foundation for growth and innovation. At our design studio, your artistic aspirations align with our dedicated support, creating a seamless path to mastery.",
-  },
-  {
-    title: "Unleash Your Creative Vision: A Personal Journey in Design Mastery",
-    description:
-      "Embark on a creative journey that’s uniquely yours. Our personalized design mentorship adapts to your individual style, setting the foundation for growth and innovation. At our design studio, your artistic aspirations align with our dedicated support, creating a seamless path to mastery.",
-  },
-  {
     title: "Real-Time Feedback & Collaboration",
     description:
       "Immerse yourself in an interactive design experience where feedback is immediate, just like working with clients on live projects. This approach enhances your understanding of design principles and empowers you to make creative decisions with confidence.",
@@ -27,6 +17,11 @@ const musicSchoolContent = [
     title: "Innovative Design Curriculum",
     description:
       "Our curriculum is continuously updated to incorporate the latest design trends, tools, and techniques, ensuring you're always working with cutting-edge knowledge. Say goodbye to outdated approaches and welcome an education that evolves with the design industry.",
+  },
+  {
+    title: "Unleash Your Creative Vision: A Personal Journey in Design Mastery",
+    description:
+      "Embark on a creative journey that’s uniquely yours. Our personalized design mentorship adapts to your individual style, setting the foundation for growth and innovation. At our design studio, your artistic aspirations align with our dedicated support, creating a seamless path to mastery.",
   },
   {
     title: "Endless Creative Possibilities",
